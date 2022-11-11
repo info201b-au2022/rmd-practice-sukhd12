73ec141b0ac511ffdca003947c75dbe3626fe6de
sukhd12 <skd26@uw.edu>
2022-11-11 12:33:29 -0800

    map html

--- source/analysis.R
+++ source/analysis.R
@@ -30,3 +30,35 @@ cases_plot <- ggplot(data = national) +
   geom_line(mapping = aes(x = as.Date(date), y = cases)) +
   labs(x = "Date", y = "Cumulative Cases", title = "U.S. COVID Cases")
 
+ballot_locations <- read.csv("/Users/sukhmandhillon/Library/CloudStorage/OneDrive-UW/Undergrad/2.Sophomore/Autumn 22/Info 201/Voting_Locations_and_Ballot_Boxes.R")
+View(ballot_locations)
+
+state <- map_data("state")
+washington <- subset(state, region == "washington")
+counties <- map_data("county")
+washington_county <- subset(counties, region == "washington")
+
+filtered_counties <- data.frame(
+  county = ballot_locations$County, type = ballot_locations$Type, lat = ballot_locations$Lat, long = ballot_locations$Long
+)
+View(filtered_counties)
+
+overall_map <- ggplot(washington) +
+  geom_polygon(mapping = aes(x = long, y = lat, group = group)) + 
+  geom_point(
+    data = filtered_counties, 
+    mapping = aes(x = long, y = lat, color = type),
+    pch = 10
+  )+
+  scale_color_manual(values=c('Light Blue','Red'))+
+  labs(title = "Voting Locations and Type") +
+  coord_map() # use a map-based coordinate system
+overall_map
+
+complete_map <- overall_map +
+  geom_polygon(data = washington_county, mapping = aes(x = long, y = lat, group=group), fill = NA, color = "white") +
+  geom_polygon(fill = NA, mapping = aes(x = long, y = lat))
+complete_map
+
+complete_map <- complete_map + labs(x = "Longitude", y = "Latitude")
+
